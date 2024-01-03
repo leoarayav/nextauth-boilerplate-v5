84c71b31b7ef8025d929bd71c9bed37927d9be82
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-03 03:21:26 -0300

    feat(page): initial home page style

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-r from-neutral-500 to-neutral-800">
-      
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-r from-neutral-600 to-neutral-950">
+      <div className="space-y-6 text-center">
+        <h1 className="text-4xl font-semibold text-white drop-shadow-md">
+          🌌 Authentication
+        </h1>
+        <p className="text-white text-lg">
+          A Next.js starter boilerplate with authentication service
+        </p>
+      </div>
     </main>
   )
 }
