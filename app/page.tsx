c4b1c4f78c6cac1466fd3ae0814acf20c3fd8994
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-03 03:34:47 -0300

    feat(page): implementation of login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-gradient-to-r from-neutral-600 to-neutral-950">
@@ -8,6 +11,11 @@ export default function Home() {
         <p className="text-white text-lg">
           A Next.js starter boilerplate with authentication service
         </p>
+        <div>
+          <LoginButton asChild>
+            <Button variant="outline" size="lg">Login</Button>
+          </LoginButton>
+        </div>
       </div>
     </main>
   )
