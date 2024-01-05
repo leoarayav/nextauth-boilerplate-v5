e551f77aa4beff23ca1cf14de4dc5014d9cb03f4
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-05 03:11:16 -0300

    feat(auth.login): using login form

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,9 @@
+import { LoginForm } from "@/components/auth/login-form";
+
 export default function LoginPage() {
   return (
     <div>
-      <p>Login page</p>
+      <LoginForm />
     </div>
   )
 }
