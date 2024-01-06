2ff2d94f4a66582e1d459fb7b3977f532c3c79dd
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-06 01:27:16 -0300

    feat(components.auth): back-button component

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+interface BackButtonProps {
+  href: string;
+  label: string;
+}
+
+export const BackButton = ({
+  href,
+  label,
+}: BackButtonProps): React.ReactElement => {
+  return <Button>{/* TODO: Complete this component */}</Button>;
+};
