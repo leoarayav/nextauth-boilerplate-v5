bdfd13184ba94053865a45403a7eecf64ea2b487
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-07 14:00:58 -0300

    feat(components.auth): login form email address field done

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -11,6 +13,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 export const LoginForm = (): React.ReactElement => {
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -27,7 +30,29 @@ export const LoginForm = (): React.ReactElement => {
       backButtonHref="/auth/register"
       showSocial
     >
-      Login form
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="xyz@gmail.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
