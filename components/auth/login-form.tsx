07ae29db77b34b57273909c3bff33b160264a49b
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-07 03:47:45 -0300

    feat(components.auth): hooking schema

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,25 @@
-import { CardWrapper } from "./card-wrapper";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export const LoginForm = (): React.ReactElement => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <CardWrapper
       headerLabel="Welcome back!"
