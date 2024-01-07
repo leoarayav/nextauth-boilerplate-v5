78d71ddef08c21c03576af0844572805538db964
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-07 14:10:54 -0300

    feat(schemas): limiting

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,6 +4,10 @@ import * as z from "zod"
  * @description Login schema for validation
  */
 export const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string() 
+  email: z.string().email({
+    message: "Please enter a valid email address"
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long"
+  }) 
 })
