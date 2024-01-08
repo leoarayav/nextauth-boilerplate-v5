28371c1840fc7653bc0702ebf924dcb18db20511
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-07 21:38:50 -0300

    feat(actions.login): first add

---
+++ actions/login.ts
@@ -0,0 +1,10 @@
+"use server";
+
+/**
+ * @description Login a user
+ * @param {object} values - The values to login with
+ * @returns {void}
+ */
+export const login = (values: any) => {
+  console.log(values);
+}
