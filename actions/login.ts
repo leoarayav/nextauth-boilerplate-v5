7c433841fec092a976fa2a0cfbf3378999a95cca
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-11 02:58:42 -0300

    feat(actions.login): fields validation

--- actions/login.ts
+++ actions/login.ts
@@ -1,10 +1,18 @@
 "use server";
 
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+
 /**
  * @description Login a user
  * @param {object} values - The values to login with
  * @returns {void}
  */
-export const login = (values: any) => {
-  console.log(values);
+export const login = (values: z.infer<typeof LoginSchema>) => {
+  const validateFields = LoginSchema.safeParse(values);
+  if (!validateFields.success)
+    return {
+      error: "Invalid fields!"
+    }
+  return { success: "Email sent!" }
 }
