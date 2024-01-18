4927f16cc3f63eec279073b29359ef7cab5c3f94
leoarayav <leandro.araya08@inacapmail.cl>
2024-01-17 23:10:55 -0300

    feat(schemas): register schema object

--- schemas/index.ts
+++ schemas/index.ts
@@ -11,3 +11,18 @@ export const LoginSchema = z.object({
     message: "Password must be at least 8 characters long"
   }) 
 })
+
+/**
+ * @description Register schema for validation
+ */
+export const RegisterSchema = z.object({
+  name: z.string().min(3, {
+    message: "Name must be at least 3 characters long"
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address"
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long"
+  }),
+})
